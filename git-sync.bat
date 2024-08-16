a4e076b0c7a2371cf899db298651d6f1ecc94760
Zian Qin <zqin1@tudelft.nl>
2024-08-16 15:12:52 +0200

    Auto-sync: Fetch, merge, add, commit, and push local changes

--- git-sync.bat
+++ git-sync.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files\Git\bin\bash.exe" -c "/c/Users/zqin1/OneDrive/MyWorkSpace/July13_2020/zqin1/MyGitHubLocalFiles/git-sync.sh"
+"C:\Users\zqin1\AppData\Local\Programs\Git\git-bash.exe" -c "/c/Users/zqin1/OneDrive/MyWorkSpace/July13_2020/zqin1/MyGitHubLocalFiles/git-sync.sh"
